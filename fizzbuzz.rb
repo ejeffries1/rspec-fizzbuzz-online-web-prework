09c01d5d06bbc474c8edd3ac0b0912dd5b311d43
Evan Jeffries <ejeffries14@gmail.com>
2019-06-02 14:50:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,7 @@
- def fizzbuzz(int)
-if int % 3 == 0 # if the number int is divisible by 3
-     int = "Fizz" # Go fizz
-
-
-  elsif int % 5 == 0 
-   int ="Buzz"
-
-
-  elsif int % 15 == 0
-  int = "FizzBuzz"
-
-
-  else 
-#  puts "not divisible by 3 or 5"
-
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
   end
-
+end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
