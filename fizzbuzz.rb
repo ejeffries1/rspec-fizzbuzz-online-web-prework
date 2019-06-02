d613ea5097a39db01bd7e831c4b11603dbe5113c
Evan Jeffries <ejeffries14@gmail.com>
2019-06-02 14:48:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,3 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
-  end
 
-  if int % 5 == 0
-   return "Buzz"
-  end
-
-  if int % 3 == 0
-   return "Fizz"
-  end
-
-end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
